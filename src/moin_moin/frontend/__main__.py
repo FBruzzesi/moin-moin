cd61bfe5979bd1c74d5d750667ed4b96cf9a84c1
Fabrizio Damicelli <fabridamicelli@gmail.com>
2024-11-22 15:43:21 +0100

    make location work with map

--- src/moin_moin/frontend/__main__.py
+++ src/moin_moin/frontend/__main__.py
@@ -1,8 +1,36 @@
 # streamlit run src/moin_moin/frontend/__main__.py
+from typing import NamedTuple
 
-import numpy as np
+import pandas as pd
 import streamlit as st
 from PIL import Image
+from geopy.geocoders import Nominatim
+
+GEOLOCATOR = Nominatim(user_agent="location_sharing_app")
+TAGS = [
+    "Street",
+    "Lights",
+    "Rubish",
+    "Crime",
+    "Broken",
+]
+
+
+def load_image(image):
+    return Image.open(image).convert("RGB")
+
+
+def parse_location(raw_loc: str):
+    if not raw_loc:
+        print("No location given")
+        return None
+    loc = GEOLOCATOR.geocode(raw_loc)
+    return loc
+
+
+def get_prediction(img, tags, notes):
+    # TODO: call model Prediction
+    return "A very accurate prediction"
 
 
 def main() -> None:
@@ -12,43 +40,32 @@ def main() -> None:
         label="Upload an image",
         type=["png", "jpg"],
     )
-    location = st.sidebar.text_input(label="Location")
-    tags = st.sidebar.selectbox(label="Tags", options=["Tag 1", "Tag 2", "Tag 3"])
+
+    raw_loc = st.sidebar.text_input(
+        "Enter your location (e.g., city name, street, number):"
+    )
+    loc = parse_location(raw_loc)
+
+    if loc:
+        st.write(loc)
+    else:
+        st.write("Location not found. Please try again.")
+
+    tags = st.sidebar.multiselect("Select tags for the image:", TAGS)
+
     notes = st.sidebar.text_area(
         label="Additional information",
         placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit",
     )
 
-    # Submit button
     if st.sidebar.button("Submit"):
-        # Process the form data here
         if image is not None:
-            st.write("Image uploaded:", image.name)
-            pilimage = Image.open(image)
-            arr_img = np.asarray(pilimage)
-            print(arr_img.shape, arr_img)
+            st.write("This is your image:")
+            img = load_image(image)
+            st.image(img)
+    st.map(data=pd.DataFrame({"lat": [loc.latitude], "lon": [loc.longitude]}))
 
-        st.write("Location:", location)
-        st.write("Tags:", tags)
-        st.write("Additional information:", notes)
-
-        # Geolocate the location
-        # geolocator = Nominatim(user_agent="geoapiExercises")
-        # location_data = geolocator.geocode(location)
-
-        # if location_data:
-        #     latitude = location_data.latitude
-        #     longitude = location_data.longitude
-        #     st.write("Coordinates:", (latitude, longitude))
-
-        #     # Create a map
-        #     map_ = folium.Map(location=[latitude, longitude], zoom_start=12)
-        #     folium.Marker([latitude, longitude], popup=location).add_to(map_)
-
-        #     # Display the map on the main page
-        #     st_folium(map_, width=700, height=500)
-        # else:
-        #     st.write("Location not found")
+    st.write("Prediction: ", get_prediction(image, tags, notes))
 
 
 if __name__ == "__main__":
